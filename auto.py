a52ed6849a548f6e013e95e258f8d826786e964a
bern <e.b.co.90@gmail.com>
2020-06-14 08:57:43 +0900

    Script to click with Pyautogui.

---
+++ auto.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+print(pyautogui.position())
+
+pyautogui.click(235, 83)
+pyautogui.hotkey("command", "c")
+pyautogui.hotkey("command", "space")
+# pyautogui.typewrite("Hello World")
+# pyautogui.typewrite("enter")
